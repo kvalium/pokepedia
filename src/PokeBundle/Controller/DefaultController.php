012fce5508d8b8f0f9c279ae36ab69aa00285a46
KValium <julien.monchany@gmail.com>
2017-01-25 20:51:46 +0100

    templating, search form and route logic

--- src/PokeBundle/Controller/DefaultController.php
+++ src/PokeBundle/Controller/DefaultController.php
@@ -4,23 +4,81 @@ namespace PokeBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use PokePHP\PokeApi;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /** @var  Form $searchForm */
+    private $searchForm;
+
     /**
-     * @Route("/")
+     * @Route("/", name="home")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('PokeBundle:Default:index.html.twig');
+
+        $form = $this->getSearchForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            if ($data['name']) {
+                return $this->redirect($this->generateUrl(
+                    'search_results',
+                    array('pattern' => $data['name'])
+                ));
+            }
+        }
+        return $this->render('PokeBundle:full:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
+
     /**
-     * @Route("/search/{pattern}")
+     * Returns the search form instance or create it
+     * @return Form
      */
-    public function searchAction($pattern){
+    private function getSearchForm()
+    {
+        if (!isset($this->searchForm)) {
+            $defaultData = array('message' => 'Type your message here');
+            /** @var Form $form */
+            $form = $this->createFormBuilder($defaultData)
+                ->add('name', TextType::class)
+                ->add('send', SubmitType::class, array('label' => 'Search'))
+                ->getForm();
+            return $form;
+        }
+        return $this->searchForm;
+    }
+
+    /**
+     * Search Results page action
+     *
+     * @Route("/search/{pattern}", name="search_results")
+     * @param $pattern
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function searchAction($pattern)
+    {
         $redis = $this->get('snc_redis.default');
-        var_dump($redis->keys($pattern.'*'));
-        return $this->render('PokeBundle:Default:index.html.twig');
+        return $this->render('PokeBundle:full:search_results.html.twig',
+            array(
+                'searchResults' => $redis->keys($pattern . '*'),
+            )
+        );
+    }
+
+    /**
+     * @Route("/details/{{name}}", name="pokemon_details")
+     */
+    public function detailsActions($name)
+    {
+
     }
 }
